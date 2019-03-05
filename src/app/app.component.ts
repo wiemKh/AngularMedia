290074cba501fad9747f4b09375adfb54540d8c2
wiem khelifi <wiam@etcinfo.fr>
2019-03-05 14:34:15 +0100

    add video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { setHours, setMinutes, setSeconds, addSeconds } from 'date-fns';
 declare var MediaRecorder: any;
 
 @Component({
@@ -14,6 +15,16 @@ export class AppComponent {
   getUrlAudio: any
   myaudiourl: any
   media: any
+  displayVideoPlayer:any
+  displayStopVideo:any
+  video:any
+  MediaRecorderobj:any
+  recordedChunks:any
+  stream1:any
+  currettime:any
+  refreshIntervalId:any
+  getBlobVideo:any
+  myurl:any
   title = 'AngularMedia';
 
   constructor(
@@ -54,6 +65,48 @@ export class AppComponent {
   stopRecord() {
     this.media.stop();
     this.displayAudioPlayer = true;
+  }
+
+   /* Enregistrement video */
+   startVideo() {
+    this.displayVideoPlayer = false;
+    this.displayStopVideo = true
+    this.video = <HTMLVideoElement>document.getElementById('video');
+    let self = this
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function (stream) {
+        self.MediaRecorderobj = new MediaRecorder(stream, { mimeType: "video/webm" });
+        self.recordedChunks = [];
+        self.MediaRecorderobj.ondataavailable =
+          (event) => { self.recordedChunks.push(event.data); };
+        self.MediaRecorderobj.start(100);
+        // self.video.src = window.URL.createObjectURL(stream); //createObjectURL is deprecated for Google Chrome
+        self.video.srcObject = stream;
+        self.stream1 = stream;
+        self.video.play();
+
+        self.video.muted = false;
+      });
+    }
+    this.currettime = setHours(setMinutes(setSeconds(new Date(), 0), 0), 0)
+    this.refreshIntervalId = setInterval(function () {
+      self.currettime = addSeconds(self.currettime, 1)
+    }, 1000);
+  }
+  /* terminer le record video */
+  stopVideo() {
+    this.stream1.getTracks().forEach(track => { track.stop(); });
+    var blob = new Blob(this.recordedChunks, { type: "video/webm" });
+    this.getBlobVideo = blob
+    var url = URL.createObjectURL(blob);
+
+    this.myurl = this.sanitizer.bypassSecurityTrustResourceUrl(url)
+    clearInterval(this.refreshIntervalId)
+    this.currettime = 0
+    let video = <HTMLVideoElement>document.getElementById('video');
+    video.pause();
+    this.displayVideoPlayer = true
+  //  this.uploadVideo(event)
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MomentModule } from "angular2-moment";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MomentModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
